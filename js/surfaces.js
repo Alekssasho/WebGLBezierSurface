9b582cb7d378d4f17af687cf23f00136808bb89c
Alekssasho <aleksandar.angelovv@gmail.com>
2018-07-02 22:08:40 +0300

    Added parametric surface which is plane for now.
    Added lights

--- js/surfaces.js
+++ js/surfaces.js
@@ -12,6 +12,7 @@ var pointCloud = [];
 // Globals params
 var params = {
     gridSize: 4,
+    subdivisionCount: 1,
 }
 
 function onWindowResize() {
@@ -32,7 +33,16 @@ function setupRendererAndStats()
     document.body.appendChild(stats.dom);
 
     scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x001111);
     finalScene = new THREE.Scene();
+
+    var ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+    finalScene.add(ambientLight);
+
+    light = new THREE.PointLight(0xffffff, 0.6);
+    light.position.set(0, 0, 5);
+    finalScene.add(light);
+    finalScene.background = new THREE.Color(0x110011);
 
     window.addEventListener( 'resize', onWindowResize, false );
 }
@@ -68,9 +78,13 @@ function setupGUI()
     gui.add(params, 'gridSize', 2, 10, 1).onChange(function(value) { 
         setupPoints(value);
         setupRaycaster();
-     });
+    });
 
-     gui.add(buttons, 'resetPoints');
+    gui.add(params, 'subdivisionCount', 1, 10, 1).onChange(function(value) {
+        // TODO:
+    });
+
+    gui.add(buttons, 'resetPoints');
 }
 
 function createSinglePoint(x, y, z)
@@ -102,55 +116,75 @@ function setupPoints(gridSize)
     }
 }
 
-var lines = [];
+var lines = null;
+function setupLinesBetweenPoints()
+{
+    if (lines != null)
+    {
+        scene.remove(lines);
+    }
+
+    let pts = [];
+    for(let y = 0; y < params.gridSize; y++)
+    {
+        for(let x = 0; x < params.gridSize - 1; x++)
+        {
+            pts.push(pointCloud[y * params.gridSize + x].position);
+            pts.push(pointCloud[y * params.gridSize + x + 1].position);
+        }
+    }
+
+    for(let x = 0; x < params.gridSize; x++)
+    {
+        for(let y = 0; y < params.gridSize - 1; y++)
+        {
+            pts.push(pointCloud[y * params.gridSize + x].position);
+            pts.push(pointCloud[(y + 1) * params.gridSize + x].position);
+        }
+    }
+
+    var geometry = new THREE.BufferGeometry().setFromPoints(pts);
+    var material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+    lines = new THREE.LineSegments(geometry, material);
+    scene.add(lines);
+}
+
+var surface = null;
+function setupSurface()
+{
+    if(surface != null)
+    {
+        finalScene.remove(surface);
+    }
+
+    let size = params.gridSize * params.subdivisionCount;
+
+    function getSurfacePoint(u, v, target) {
+        var x = Math.floor(u * (size - 1));
+        var y = Math.floor(v * (size - 1));
+        var x1 = Math.ceil(u * (size - 1));
+        var y1 = Math.ceil(v * (size - 1));
+        var pos = pointCloud[y * params.gridSize + x].position;
+        var pos1 = pointCloud[y1 * params.gridSize + x1].position;
+        var result = new THREE.Vector3();
+        target.set(
+            THREE.Math.lerp(pos.x, pos1.x, x1 - u * (size - 1)),
+            THREE.Math.lerp(pos.y, pos1.y, y1 - v * (size - 1)),
+            pos.z);
+    }
+
+    let geometry = new THREE.ParametricGeometry(getSurfacePoint, size - 1, size - 1);
+    let material = new THREE.MeshPhongMaterial({color: 0x00ff00, side: THREE.DoubleSide });
+    surface = new THREE.Mesh(geometry, material);
+    finalScene.add(surface);
+
+    light.target = surface;
+}
+
 function render()
 {
-    if (lines.length > 0)
-    {
-        for(let l of lines)
-        {
-            scene.remove(l);
-            finalScene.remove(l);
-        }
-    }
-
-    for(let i = 0; i < params.gridSize; i++)
-    {
-        let pts = [];
-        for(let y = 0; y < params.gridSize; y++)
-        {
-            pts.push(pointCloud[i * params.gridSize + y].position);
-        }
-        var curve = new THREE.CatmullRomCurve3(pts);
-        var p = curve.getPoints(50);
-        var geometry = new THREE.BufferGeometry().setFromPoints(p);
-        var material = new THREE.LineBasicMaterial({ color: 0xff0000 });
-        var line = new THREE.Line(geometry, material);
-        var clone = line.clone();
-        lines.push(clone);
-        lines.push(line);
-        scene.add(line);
-        finalScene.add(clone);
-    }
-
-    for(let i = 0; i < params.gridSize; i++)
-    {
-        let pts = [];
-        for(let y = 0; y < params.gridSize; y++)
-        {
-            pts.push(pointCloud[y * params.gridSize + i].position);
-        }
-        var curve = new THREE.CatmullRomCurve3(pts);
-        var p = curve.getPoints(50);
-        var geometry = new THREE.BufferGeometry().setFromPoints(p);
-        var material = new THREE.LineBasicMaterial({ color: 0xff0000 });
-        var line = new THREE.Line(geometry, material); 
-        var clone = line.clone();
-        lines.push(line);
-        lines.push(clone);
-        scene.add(line);
-        finalScene.add(clone);
-    }
+    setupLinesBetweenPoints();
+    setupSurface();
 
     renderer.setViewport(0, 0, window.innerWidth / 2, window.innerHeight);
     renderer.setScissor(0, 0, window.innerWidth / 2, window.innerHeight);
