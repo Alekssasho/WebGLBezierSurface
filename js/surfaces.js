f3b49245b3a08f5a172f874ed3ca1d51df3e6bfe
Alekssasho <aleksandar.angelovv@gmail.com>
2018-07-02 08:59:04 +0300

    Added grid of points and curves between them

--- js/surfaces.js
+++ js/surfaces.js
@@ -32,7 +32,7 @@ function setupRendererAndStats()
 function setupCamera()
 {
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 15;
+    camera.position.z = 10;
     controls = new THREE.OrbitControls( camera );
     controls.enableZoom = false;
 }
@@ -44,49 +44,73 @@ function setupRaycaster()
     dragControls.addEventListener( 'dragend', function ( event ) { controls.enabled = true; } );
 }
 
-function setupPoints() 
+function createSinglePoint(x, y, z)
 {
-    var points = [
-        new THREE.Vector3(0, 0, 0),
-        new THREE.Vector3(1, 0, 0),
-        new THREE.Vector3(0, 1, 0),
-        new THREE.Vector3(1, 1, 0)
-    ];
+    var pointGeometry = new THREE.Geometry();
+    pointGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
+    pointGeometry.computeBoundingBox();
+    var pointMaterial = new THREE.PointsMaterial({size: 0.5, color: new THREE.Color(1, 1, 1) });
+    var point = new THREE.Points(pointGeometry, pointMaterial);
+    point.position.set(x, y, z);
+    return point;
+}
 
-    for(var i = 0; i < points.length; i++)
+function setupPoints(gridSize)
+{
+    for(var x = 0; x < gridSize; x++)
     {
-        var pointGeometry = new THREE.Geometry();
-        pointGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
-        pointGeometry.computeBoundingBox();
-        var pointMaterial = new THREE.PointsMaterial({size: 0.5, color: new THREE.Color(1, 1, 1) });
-        var point = new THREE.Points(pointGeometry, pointMaterial);
-        point.position.set(points[i].x, points[i].y, points[i].z);
-
-        pointCloud.push(point);
-        scene.add(point);
+        for(var y = 0; y < gridSize; y++)
+        {
+            var pt = createSinglePoint(x - gridSize / 2, y - gridSize / 2, 0);
+            pointCloud.push(pt);
+            scene.add(pt);
+        }
     }
 }
 
-
-var line = null;
+var gridSize = 4;
+var lines = [];
 function render()
 {
-    if (line != null)
+    if (lines.length > 0)
     {
-        scene.remove(line);
+        for(let l of lines)
+        {
+            scene.remove(l);
+        }
     }
 
-    var curve = new THREE.CubicBezierCurve3(
-        pointCloud[0].position,
-        pointCloud[1].position,
-        pointCloud[2].position,
-        pointCloud[3].position
-    );
-    var p = curve.getPoints(50);
-    var geometry = new THREE.BufferGeometry().setFromPoints(p);
-    var material = new THREE.LineBasicMaterial({ color: 0xff0000 });
-    line = new THREE.Line(geometry, material);
-    scene.add(line);
+    for(let i = 0; i < gridSize; i++)
+    {
+        var curve = new THREE.CubicBezierCurve3(
+            pointCloud[i * gridSize + 0].position,
+            pointCloud[i * gridSize + 1].position,
+            pointCloud[i * gridSize + 2].position,
+            pointCloud[i * gridSize + 3].position
+        );
+        var p = curve.getPoints(50);
+        var geometry = new THREE.BufferGeometry().setFromPoints(p);
+        var material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+        var line = new THREE.Line(geometry, material); 
+        lines.push(line);
+        scene.add(line);
+    }
+
+    for(let i = 0; i < gridSize; i++)
+    {
+        var curve = new THREE.CubicBezierCurve3(
+            pointCloud[0 * gridSize + i].position,
+            pointCloud[1 * gridSize + i].position,
+            pointCloud[2 * gridSize + i].position,
+            pointCloud[3 * gridSize + i].position
+        );
+        var p = curve.getPoints(50);
+        var geometry = new THREE.BufferGeometry().setFromPoints(p);
+        var material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+        var line = new THREE.Line(geometry, material); 
+        lines.push(line);
+        scene.add(line);
+    }
 
     renderer.render( scene, camera );
 }
@@ -95,7 +119,7 @@ function main()
 {
     setupRendererAndStats();
     setupCamera();
-    setupPoints();
+    setupPoints(gridSize);
     setupRaycaster();
 
     function loop() {
